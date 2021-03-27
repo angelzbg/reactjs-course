4ccb6543b9886e268670dcfd0e6a4b2d773ae24d
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2021-03-27 20:11:12 +0200

    backend routing
    backend routing

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,3 +1,11 @@
+const jwt = require('jsonwebtoken');
+
 const getId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
 
-module.exports = { getId };
+const signToken = (login) => {
+  const payload = { login };
+  const token = jwt.sign(payload, secret /*, { expiresIn: '1h' }*/);
+  return token;
+};
+
+module.exports = { getId, signToken };
