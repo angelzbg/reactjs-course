9119154329db9d8b5fc5a5ea5f8f3436862dc85a
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2021-03-27 20:12:23 +0200

    Update utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,11 +1,3 @@
-const jwt = require('jsonwebtoken');
-
 const getId = () => Date.now().toString(36) + Math.random().toString(36).substring(2);
 
-const signToken = (login) => {
-  const payload = { login };
-  const token = jwt.sign(payload, secret /*, { expiresIn: '1h' }*/);
-  return token;
-};
-
-module.exports = { getId, signToken };
+module.exports = { getId };
